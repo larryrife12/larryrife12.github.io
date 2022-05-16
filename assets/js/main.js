059c30d47dcc935186e612e0ed28fd7145fa32eb
larryrife12 <105509250+larryrife12@users.noreply.github.com>
2022-05-16 00:18:31 -0400

    Added formspree javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,41 @@
 (function() {
   "use strict";
 
+  // form via formspree
+  var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.classList.add('sent-message')
+          status.innerHTML = "Your message has been sent. Thank you!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.classList.add('error-message')
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.classList.add('error-message')
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
   /**
    * Easy selector helper function
    */
